6715c8e5ab7d7a1af1f92d9ce4b1713b341b256a
pope1280 <dbc@dbc.com>
2013-06-11 18:55:42 -0700

    first commit

---
+++ application.js
@@ -0,0 +1,50 @@
+
+// Die related //
+
+function Die(index, sides) {
+  this.index = index;
+  this.sides = sides;
+  this.value = 0;
+  addDieToDom();
+}
+
+Die.prototype.roll = function() {
+  return this.value = Math.floor((Math.random()*this.sides)+1);
+};
+
+
+
+// DOM Related ..... should these be prototypical? //
+
+function addDieToDom() {
+  $('.dice').append('<div class="die">0</div>');
+}
+
+function updateDomDieValue(k, value) {
+  var domIndex = k+1;
+  $('.dice').find('.die:nth-child('+domIndex+')').text(value);
+}
+
+
+
+// Event listeners //
+
+$(document).ready(function() {
+  var dice = [];
+  var index = 0;
+
+  $('#roller button.add').on('click', function() {
+    var die = new Die(index, 6);
+    dice.push(die);
+    index++;
+  });
+
+  $('#roller button.roll').on('click', function() {
+    for (var k = 0; k < dice.length; k++)
+    {
+    var die = dice[k];
+    var value = die.roll();
+    updateDomDieValue(k, value);
+    }
+  });
+});
